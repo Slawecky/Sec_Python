7014f84b886e0abe987a6f1f4c0f8939b8fda2e8
Slawecky <comsite@comsite.com.pl>
2018-02-03 19:46:16 +0100

    Head First Second

--- Head_First/beersong.py
+++ Head_First/beersong.py
@@ -5,13 +5,13 @@ for beer_num in range(7, 0, -1):
     print(beer_num, word, "Piwa na ścianie.")
     print(beer_num, word, "Piwa.")
     print("Jedno weź.")
-    print("podaj w koło.")
+    print("Podaj w koło.")
     if beer_num == 1:
         print("nie ma już butelek piwa na ścianie.")
     else:
         new_num = beer_num - 1
         if new_num == 1:
             word = "Butelka."
-         print(new_num, word, "Piwa na ścianie.")
+            print(new_num, word, "Piwa na ścianie.")
     print()
 print("Do jutra")
